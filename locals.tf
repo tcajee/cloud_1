9114bf86203602f1abe187c2db6f1ca15ac35098
Tameem Cajee <tameem.cajee@gmail.com>
2020-12-02 15:07:03 +0200

    cleanup, preparing for helm deployment

--- locals.tf
+++ locals.tf
@@ -1,17 +1,12 @@
  locals {
 
-   subnets = {
-     main = {
-       public = {
-         name = "wp"
-         cidr = "10.2.0.0/16"
-       }
-     }
-   }
- 
-   # example common tags which should be applied to all resources for cost tracking ect.
-   common_tags = {
-     project_name = var.project_name
-     cost_centre  = "cld1cc"
-   }
+  subnets = {
+    main = {
+      public = {
+        name = "wp"
+        cidr = "10.2.0.0/16"
+      }
+    }
   }
+
+}
