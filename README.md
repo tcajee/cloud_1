5230be8c2c5f45ed40a62f96e0c4b2941c5b2fdf
Tameem Cajee <tameem.cajee@gmail.com>
2020-12-08 10:25:16 +0200

    wordpress deployment functional

--- README.md
+++ README.md
@@ -1,53 +1,72 @@
 # Cloud_1
-This repository contains a Terraform configuration to deploy the neceassary cloud infrastrucutre required for the DevOps module at WeThinkCode.
 
-## Pre-requisites
-- Install [`terraform`](https://learn.hashicorp.com/tutorials/terraform/install-cli)
-- Install [`gcloud`](https://cloud.google.com/sdk/docs/install)
-- Install [`kubectl`](https://kubernetes.io/docs/tasks/tools/install-kubectl/)
-- Install [`helm`](https://helm.sh/)
+This repository contains a Terraform configuration to deploy the neceassary cloud infrastrucutre required for the DevOps module at WeThinkCode. The configuration files in the root folder deploy a GKE Public Cluster and a WordPress chart using [Helm](https://helm.sh/).
 
-## Configuration
-- Obtain the terraform-gcp-credentials.json file containing the required service account credentials for the project.
+## Overview
 
-## Terraform
--To initialise Terraform workSpace: `terraform init`
--To create infrastructure: `terraform apply`
--To delete infrastructure: `terraform destroy`
+Follow these step to get up and running with GKE and Helm:
 
-### Helm
-Helm is a package maneger for Kubernetes that we use to install WordPress on our Kubernetes cluster using. It also configures the load balancer and auto-scaler that will be used in the project.
+1. [Install the necessary tools](#installing-necessary-tools)
+1. [Apply the Terraform code](#apply-the-terraform-code)
+1. [Verify the Deployed Chart](#verify-the-deployed-chart)
+1. [Destroy the Deployed Resources](#destroy-the-deployed-resources)
+
+## Installing necessary tools
+
+In addition to `terraform`, this configuration relies on `gcloud` and `kubectl` and `helm` tools to manage the cluster.
+
+This means that your system needs to be configured to be able to find `terraform`, `gcloud`, `kubectl` and `helm`
+client utilities on the system `PATH`. Here are the installation guides for each tool:
+
+1. [`gcloud`](https://cloud.google.com/sdk/gcloud/)
+1. [`kubectl`](https://kubernetes.io/docs/tasks/tools/install-kubectl/)
+1. [`terraform`](https://learn.hashicorp.com/terraform/getting-started/install.html)
+1. [`helm`](https://docs.helm.sh/using_helm/#installing-helm) (Minimum version v3.0)
+
+Make sure the binaries are discoverable in your `PATH` variable. See [this Stack Overflow
+post](https://stackoverflow.com/questions/14637979/how-to-permanently-set-path-on-linux-unix) for instructions on
+setting up your `PATH` on Unix, and [this
+post](https://stackoverflow.com/questions/1618280/where-can-i-set-path-to-make-exe-on-windows) for instructions on
+Windows.
+
+## Apply the Terraform Code
+
+Once all the prerequisite tools are installed, you may proceed to deploy the GKE cluster using these steps:
+
+1. Make sure you are in the root of the repository.
+1. Fill in the required variables in `variables.tf`.
+1. Authenticate to GCP:
+   - `gcloud auth login`
+   - `gcloud auth application-default login`
+1. Initialize terraform:
+   - `terraform init`
+1. Check the terraform plan:
+   - `terraform plan`
+1. Apply the terraform code:
+   - `terraform apply`
 
 
-## Troubleshooting
+## Verify the Deployed Chart
 
-### Verifying kubectl configuration
+At the end of the `terraform apply`, you should now have a working GKE cluster and `kubectl` context configured.
+The deployment configures your `kubectl` context, so you can use `kubectl` and `helm` commands without further configuration.
 
-In order for kubectl to find and access a Kubernetes cluster, it needs a kubeconfig file, which is created automatically when you create a cluster using kube-up.sh or successfully deploy a Minikube cluster.
+To see the created resources, run the following commands:
 
-By default, kubectl configuration is located at ~/.kube/config.
-
--Check that kubectl is properly configured by getting the cluster state:
 ```bash
-kubectl cluster-info
+❯ kubectl get deployments -n default
+❯ kubectl get service -n default
+❯ kubectl get pods -n default
 ```
 
-If you see a URL response, kubectl is correctly configured to access your cluster.
+If you wish to access the deployed service, you use the `kubectl port-forward` command to forward a local port to the deployed service:
 
-If you see a message similar to the following, kubectl is not configured correctly or is not able to connect to a Kubernetes cluster.
-
-The connection to the server <server-name:port> was refused - did you specify the right host or port?
-
-If kubectl cluster-info returns the url response but you can't access your cluster, to check whether it is configured properly, use:
 ```bash
-kubectl cluster-info dump
+❯ kubectl port-forward deployment/wordpress 8080:8080 -n default
 ```
 
-### Notes
+You can now access the deployed service by opening your web browser to `http://localhost:8080`.
 
-Gcloud setup:
-```bash
-gcloud init
-gcloud auth login
-```
+## Destroy the deployed resources
 
+To destroy all resources created by the example, just run `terraform destroy`.
--- main.tf
+++ main.tf
@@ -1,18 +1,294 @@
-// Set Current Project in gcloud SDK
+# ---------------------------------------------------------------------------------------------------------------------
+# DEPLOY A GKE PUBLIC CLUSTER IN GOOGLE CLOUD PLATFORM WITH AN EXAMPLE CHART USING HELM
+# This is an example of how to use the gke-cluster module to deploy a public Kubernetes cluster in GCP with a
+# Load Balancer in front of it. This example also deploys a chart using Helm.
+# ---------------------------------------------------------------------------------------------------------------------
+
+terraform {
+  # This module is now only being tested with Terraform 0.13.x. However, to make upgrading easier, we are setting
+  # 0.12.26 as the minimum version, as that version added support for required_providers with source URLs, making it
+  # forwards compatible with 0.13.x code.
+  required_version = ">= 0.12.26"
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# PREPARE PROVIDERS
+# ---------------------------------------------------------------------------------------------------------------------
+
+provider "google" {
+  version = "~> 3.43.0"
+  project = var.project
+  region  = var.region
+  credentials = file("./terraform-gcp-credentials.json")
+
+  scopes = [
+    # Default scopes
+    "https://www.googleapis.com/auth/compute",
+    "https://www.googleapis.com/auth/cloud-platform",
+    "https://www.googleapis.com/auth/ndev.clouddns.readwrite",
+    "https://www.googleapis.com/auth/devstorage.full_control",
+
+    # Required for google_client_openid_userinfo
+    "https://www.googleapis.com/auth/userinfo.email",
+  ]
+}
+
+provider "google-beta" {
+  version = "~> 3.43.0"
+  project = var.project
+  region  = var.region
+
+  scopes = [
+    # Default scopes
+    "https://www.googleapis.com/auth/compute",
+    "https://www.googleapis.com/auth/cloud-platform",
+    "https://www.googleapis.com/auth/ndev.clouddns.readwrite",
+    "https://www.googleapis.com/auth/devstorage.full_control",
+
+    # Required for google_client_openid_userinfo
+    "https://www.googleapis.com/auth/userinfo.email",
+  ]
+}
+
+# We use this data provider to expose an access token for communicating with the GKE cluster.
+data "google_client_config" "client" {}
+
+
+# Error: Attempted to load application default credentials since neither `credentials` nor `access_token` was set in the provider block.  No credentials loaded. To use your gcloud credentials, run 'gcloud auth application-default login'.  Original error: google: could not find default credentials. See https://developers.google.com/accounts/docs/application-default-credentials for more information.
+
 resource "null_resource" "set_gcloud_project" {
+  # Set Current Project in gcloud SDK
   provisioner "local-exec" {
-    command = "gcloud config set project ${var.project_id}"
+    command = "gcloud config set project ${var.project}"
   }
 }
 
-// Configure Kubectl with GCP K8s Cluster
-resource "null_resource" "configure_kubectl" {
-  provisioner "local-exec" {
-    command = "gcloud container clusters get-credentials ${google_container_cluster.gke_cluster.name} --region ${google_container_cluster.gke_cluster.location} --project ${google_container_cluster.gke_cluster.project}"
+
+# Use this datasource to access the Terraform account's email for Kubernetes permissions.
+data "google_client_openid_userinfo" "terraform_user" {}
+
+provider "kubernetes" {
+  version = "~> 1.7.0"
+
+  load_config_file       = false
+  host                   = data.template_file.gke_host_endpoint.rendered
+  token                  = data.template_file.access_token.rendered
+  cluster_ca_certificate = data.template_file.cluster_ca_certificate.rendered
+}
+
+provider "helm" {
+  # Use provider with Helm 3.x support
+  version = "~> 1.1.1"
+
+  kubernetes {
+    load_config_file       = false
+    host                   = data.template_file.gke_host_endpoint.rendered
+    token                  = data.template_file.access_token.rendered
+    cluster_ca_certificate = data.template_file.cluster_ca_certificate.rendered
+  }
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# DEPLOY A PUBLIC CLUSTER IN GOOGLE CLOUD PLATFORM
+# ---------------------------------------------------------------------------------------------------------------------
+
+module "gke_cluster" {
+  # When using these modules in your own templates, you will need to use a Git URL with a ref attribute that pins you
+  # to a specific version of the modules, such as the following example:
+  # source = "github.com/gruntwork-io/terraform-google-gke.git//modules/gke-cluster?ref=v0.2.0"
+  source = "./modules/gke-cluster"
+
+  name = var.cluster_name
+
+  project  = var.project
+  location = var.location
+  network  = module.vpc_network.network
+
+  # We're deploying the cluster in the 'public' subnetwork to allow outbound internet access
+  # See the network access tier table for full details:
+  # https://github.com/gruntwork-io/terraform-google-network/tree/master/modules/vpc-network#access-tier
+  subnetwork                   = module.vpc_network.public_subnetwork
+  cluster_secondary_range_name = module.vpc_network.public_subnetwork_secondary_range_name
+
+  # To make testing easier, we keep the public endpoint available. In production, we highly recommend restricting access to only within the network boundary, requiring your users to use a bastion host or VPN.
+  disable_public_endpoint = "false"
+
+  # add resource labels to the cluster
+  resource_labels = {
+    environment = "testing"
+  }
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# CREATE A NODE POOL
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "google_container_node_pool" "node_pool" {
+  provider = google-beta
+
+  name     = "main-pool"
+  project  = var.project
+  location = var.location
+  cluster  = module.gke_cluster.name
+
+  initial_node_count = "1"
+
+  autoscaling {
+    min_node_count = "1"
+    max_node_count = "5"
   }
 
-  depends_on = [
+  management {
+    auto_repair  = "true"
+    auto_upgrade = "true"
+  }
+
+  node_config {
+    image_type   = "COS"
+    machine_type = "n1-standard-1"
+
+    labels = {
+      all-pools-example = "true"
+    }
+
+    # Add a public tag to the instances. See the network access tier table for full details:
+    # https://github.com/gruntwork-io/terraform-google-network/tree/master/modules/vpc-network#access-tier
+    tags = [
+      module.vpc_network.public,
+      "helm-wordpress",
+    ]
+
+    disk_size_gb = "30"
+    disk_type    = "pd-standard"
+    preemptible  = false
+
+    service_account = module.gke_service_account.email
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+  }
+
+  lifecycle {
+    ignore_changes = [initial_node_count]
+  }
+
+  timeouts {
+    create = "30m"
+    update = "30m"
+    delete = "30m"
+  }
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# CREATE A CUSTOM SERVICE ACCOUNT TO USE WITH THE GKE CLUSTER
+# ---------------------------------------------------------------------------------------------------------------------
+
+module "gke_service_account" {
+  # When using these modules in your own templates, you will need to use a Git URL with a ref attribute that pins you
+  # to a specific version of the modules, such as the following example:
+  # source = "github.com/gruntwork-io/terraform-google-gke.git//modules/gke-service-account?ref=v0.2.0"
+  source = "./modules/gke-service-account"
+
+  name        = var.cluster_service_account_name
+  project     = var.project
+  description = var.cluster_service_account_description
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# CREATE A NETWORK TO DEPLOY THE CLUSTER TO
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "random_string" "suffix" {
+  length  = 4
+  special = false
+  upper   = false
+}
+
+module "vpc_network" {
+  source = "github.com/gruntwork-io/terraform-google-network.git//modules/vpc-network?ref=v0.6.0"
+
+  name_prefix = "${var.cluster_name}-network-${random_string.suffix.result}"
+  project     = var.project
+  region      = var.region
+
+  cidr_block           = var.vpc_cidr_block
+  secondary_cidr_block = var.vpc_secondary_cidr_block
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# CONFIGURE KUBECTL AND RBAC ROLE PERMISSIONS
+# ---------------------------------------------------------------------------------------------------------------------
+
+# configure kubectl with the credentials of the GKE cluster
+resource "null_resource" "configure_kubectl" {
+  provisioner "local-exec" {
+    command = "gcloud beta container clusters get-credentials ${module.gke_cluster.name} --region ${var.region} --project ${var.project}"
+
+    # Use environment variables to allow custom kubectl config paths
+    environment = {
+      KUBECONFIG = var.kubectl_config_path != "" ? var.kubectl_config_path : ""
+    }
+  }
+
+  depends_on = [ 
     null_resource.set_gcloud_project,
-    google_container_cluster.gke_cluster
+    google_container_node_pool.node_pool
   ]
+}
+
+resource "kubernetes_cluster_role_binding" "user" {
+  metadata {
+    name = "admin-user"
+  }
+
+  role_ref {
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
+    api_group = "rbac.authorization.k8s.io"
+  }
+
+  subject {
+    kind      = "User"
+    name      = data.google_client_openid_userinfo.terraform_user.email
+    api_group = "rbac.authorization.k8s.io"
+  }
+
+  subject {
+    kind      = "Group"
+    name      = "system:masters"
+    api_group = "rbac.authorization.k8s.io"
+  }
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# DEPLOY A SAMPLE CHART
+# A chart repository is a location where packaged charts can be stored and shared. Define Bitnami Helm repository location,
+# so Helm can install the nginx chart.
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "helm_release" "wordpress" {
+  depends_on = [google_container_node_pool.node_pool]
+
+  repository = "https://charts.bitnami.com/bitnami"
+  name       = "wordpress"
+  chart      = "wordpress"
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# WORKAROUNDS
+# ---------------------------------------------------------------------------------------------------------------------
+
+# This is a workaround for the Kubernetes and Helm providers as Terraform doesn't currently support passing in module
+# outputs to providers directly.
+data "template_file" "gke_host_endpoint" {
+  template = module.gke_cluster.endpoint
+}
+
+data "template_file" "access_token" {
+  template = data.google_client_config.client.access_token
+}
+
+data "template_file" "cluster_ca_certificate" {
+  template = module.gke_cluster.cluster_ca_certificate
 }
